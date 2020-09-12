776d4e34e57051f61789e1916ae26d37cb8e3df2
Neda Jahadi <neda_jahadi@yahoo.com>
2020-09-12 16:05:02 +0200

    initial codes to make HamburgerMenuBtn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
+import Hamsterburen from './components/HamburgerMenuBtn';
 
 function App() {
   return (
     <div className="App">
-      <body>
+      
         <div className="splash-container">
           <h1 className="splash-title">HELLO TEAM</h1>
           <p>Hamsterburen</p>
+		  <Hamsterburen />
         </div>
-      </body>
+      
     </div>
   );
 }
---
+++ src/components/HamburgerMenuBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HamburgerMenuBtn = () => {
+
+	return(
+		<div>
+          <p>this is hamburger menu</p>
+		</div>
+	)
+}
+
+export default HamburgerMenuBtn;
