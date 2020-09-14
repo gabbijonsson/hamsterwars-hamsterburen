b6cc4f001c1e6374bd186a9fd4058f49ab25e3d3
Neda Jahadi <neda_jahadi@yahoo.com>
2020-09-14 10:03:07 +0200

    CombatantCard component is made and it's color changes with given color

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 import HamburgerMenuBtn from './components/HamburgerMenuBtn';
 import StartHeader from './components/StartHeader';
-
+import CombatantCard from './components/CombatantCard';
+import CombatantImg from './hamsterTest-1.jpg';
 
 function App() {
+
+
   return (
     <div className="App">
       
@@ -12,8 +15,9 @@ function App() {
           <h1 className="splash-title">HELLO TEAM</h1>
           <p>Hamsterburen</p> 
 		  
-		   <HamburgerMenuBtn />
-		  <StartHeader />
+		   {/* <HamburgerMenuBtn />
+		  <StartHeader /> */}
+		  <CombatantCard CombatantImg={CombatantImg} borderColor='blue' />
 		  
         </div>
       
