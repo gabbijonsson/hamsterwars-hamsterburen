25542c448d2f857f17e2bde96afc3a4d4d8f6b8a
Daniel Johannesson <daniel@johannesson.cz>
2020-09-16 15:30:16 +0200

    added burgerBtn to mobileHeader + menu to desktop

--- src/components/HeaderGeneric.jsx
+++ src/components/HeaderGeneric.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import './HeaderGeneric.css'
 import HomeIcon from './HomeIcon.svg'
+import HamburgerMenu from './HamburgerMenu'
+import HamburgerMenuBtn from './HamburgerMenuBtn'
 
 function HeaderGeneric() {
 	return (
 		<header id='HeaderGeneric'>
 			<div className="header-item-1">
 				<img src={HomeIcon} alt="App icon (to go home)"/>
+				
+				<div className="desktop-nav">
+					<HamburgerMenu/>
+				</div>
 			</div>
 			<div className="header-item-2">
 
 			</div>
 			<div className="header-item-3">
-
+				<HamburgerMenuBtn/>
 			</div>
 			
 		</header>
