a92f3e8ae13becba5c5b3c1558a5e2081e78f31e
Neda Jahadi <neda_jahadi@yahoo.com>
2020-09-17 15:38:30 +0200

    ByggBattle desktop view is made too

--- src/components/BattlePage.jsx
+++ src/components/BattlePage.jsx
@@ -8,36 +8,27 @@ import PickWinnerBtn from '../components/PickWinnerBtn';
 const BattlePage = ({img1,img2}) => {
 	return(
 		<div className="battlepage">
-			<HeaderGeneric />
-			
-				
+				<HeaderGeneric />
+            <div className="battlepage-main">
 			    <CombatantCard CombatantImg={img1} borderColor='blue'/>
-				
 
 				<div className="stack-up1">
 					<PickWinnerBtn />
 				</div>
-				
-			
-			
 			
 				<h1>VS.</h1>
-			
 
-			
-				
-					<CombatantCard CombatantImg={img2} borderColor='purple'/>
-				
+				<CombatantCard CombatantImg={img2} borderColor='purple'/>
 
 				<div className="stack-up2">
 					<PickWinnerBtn />
 				</div>
-				
+
+				<div className='generic'>
+					<GenericBtn text ='Pick your own fighter' color='teal'/>
+				</div>
 			
-			<div className='generic'>
-				<GenericBtn text ='Pick your own fighter' color='teal'/>
 			</div>
-			
 
 
 		</div>
