ab2dbb7d0ef5a310cb73c1d1a51bcacac03f8eaa
Daniel Johannesson <daniel@johannesson.cz>
2020-09-17 20:49:31 +0200

    created component, import other, init

---
+++ src/components/ResultView.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CombatantPicCard from './CombatantPicCard'
+import CombatantInfoCard from './CombatantInfoCard'
+import GenericBtn from './GenericBtn'
+
+function ResultView() {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default ResultView
