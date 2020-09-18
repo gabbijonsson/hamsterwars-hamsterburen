8c8fd1818e2a90e7afb41f787d3007a09b6d1452
Neda Jahadi <neda_jahadi@yahoo.com>
2020-09-18 09:12:44 +0200

    hamburger menu dropdown list is adjusted by view

--- src/components/BattlePage.jsx
+++ src/components/BattlePage.jsx
@@ -8,21 +8,25 @@ import PickWinnerBtn from '../components/PickWinnerBtn';
 const BattlePage = ({img1,img2}) => {
 	return(
 		<div className="battlepage">
+                
+				<div className="stack-up1">
+					<PickWinnerBtn />
+				</div>
+				<div className="stack-up2">
+					<PickWinnerBtn />
+				</div>
+			    
 				<HeaderGeneric />
             <div className="battlepage-main">
 			    <CombatantCard CombatantImg={img1} borderColor='blue'/>
 
-				<div className="stack-up1">
-					<PickWinnerBtn />
-				</div>
+				
 			
 				<h1>VS.</h1>
 
 				<CombatantCard CombatantImg={img2} borderColor='purple'/>
 
-				<div className="stack-up2">
-					<PickWinnerBtn />
-				</div>
+				
 
 				<div className='generic'>
 					<GenericBtn text ='Pick your own fighter' color='teal'/>
