e892ee65875e08bfdea773f9bcdd59118827df9a
Fanny <fanny.ahlm@gmail.com>
2020-09-18 15:50:50 +0200

    skapat switch case för views

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import StartPage from './components/StartPage';
 import CreateHamsterView from './components/CreateHamsterView';
-import HeaderGeneric from './components/HeaderGeneric'
+import BattlePage from './components/BattlePage';
 
 function App() {
+
+  const STARTPAGE = 'startpage' , BATTLEPAGE = 'battlepage' , CREATEHAMSTERPAGE = 'createhamsterpage' , STATSPAGE = 'statspage' ;
+
+  const [screen, setScreen] = useState(STARTPAGE);
+
+  let content = null;
+
+  switch(screen){
+
+    case CREATEHAMSTERPAGE:
+      content = <CreateHamsterView/>
+      break;
+
+    case BATTLEPAGE:
+      content = <BattlePage/>
+      break;
+
+    case STATSPAGE:
+      content = null
+       //Lägg till statsview här istället när klar
+      break;
+
+    default:
+      content = <StartPage/>
+  }
 
   return (
     <div className="App">
         <div className="splash-container">
 
-        <CreateHamsterView/>
+        {content}
 
         </div>
       
