96914b44d3102599d247e6c28e0d7c77eb7eaa8d
Daniel Johannesson <daniel@johannesson.cz>
2020-09-23 01:34:31 +0200

    removed genericBtn-component

--- src/components/CreateHamsterView.jsx
+++ src/components/CreateHamsterView.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import HeaderGeneric from './HeaderGeneric';
 import UploadForm from './UploadForm';
-import GenericBtn from './GenericBtn';
 
 const CreateHamsterView = () => {
     return(
         <>
             <HeaderGeneric/>
             <UploadForm/>
-            <GenericBtn text={"add"} color={"peach"}/>
         </>
     )
     
