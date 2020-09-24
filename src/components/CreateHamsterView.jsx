4529dd5a8b7084b237156a0b92f396966441cde2
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2020-09-24 14:23:07 +0200

    Update CreateHamsterView.jsx
    removed genericHeader...

--- src/components/CreateHamsterView.jsx
+++ src/components/CreateHamsterView.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import HeaderGeneric from './HeaderGeneric';
 import UploadForm from './UploadForm';
 
 const CreateHamsterView = () => {
     return(
         <>
-            <HeaderGeneric/>
             <UploadForm/>
         </>
     )
