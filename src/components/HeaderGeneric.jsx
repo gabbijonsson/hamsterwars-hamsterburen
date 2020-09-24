677e873bfdf56a1ed209c3c54277d4507ef43a8b
Fanny <fanny.ahlm@gmail.com>
2020-09-25 00:26:09 +0200

    lagt till routing, eventuellt missat något, återkommer till det imorska fortfarande till lite till

--- src/components/HeaderGeneric.jsx
+++ src/components/HeaderGeneric.jsx
@@ -4,12 +4,16 @@ import HomeIcon from './HomeIcon.svg'
 import HamburgerMenu from './HamburgerMenu'
 import HamburgerMenuBtn from './HamburgerMenuBtn'
 import DesktopHeaderBtn from './DesktopHeaderBtn'
+import {Link} from 'react-router-dom';
 
 function HeaderGeneric() {
 	return (
 		<header id='HeaderGeneric'>
 			<div className="header-item-1">
-				<img src={HomeIcon} alt="App icon (to go home)"/>
+				<Link to="/">
+					<img src={HomeIcon} alt="App icon (to go home)"/>
+				</Link>
+
 				
 				<div className="desktop-nav">
 					<HamburgerMenu/>
