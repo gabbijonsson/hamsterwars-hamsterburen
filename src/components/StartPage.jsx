ade8f08ec426a704ec6eed862af7793ab7f2c767
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2020-09-28 16:56:23 +0200

    Changed text and sizeing on startpage and menucards

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -7,29 +7,42 @@ import AddImg from '../assets/frontend/JoyfulHamster.svg';
 import {Link} from 'react-router-dom';
 
 const StartPage = () => {
-let textPart1 = "Tired of cleaning up after your hamsters MMA-rounds?" 
-let textPart2 = "Now you can have the battles in our virtual cage. Let your hamster go head to head with some of the most adorable combatants out there. Track the hamsters performance with our Statistics-tool, or add more hamsters to your roster with the upload-feature!"
-	return(
+let textPart1 = 
+	"Tired of messy hamsterfights?"; 
+let textPart2 = 
+	"Use our virtual cage.";
+let textPart3 =
+	"Let your hamster go head to head with some of the most adorable combatants out there, and track their stats from previous fights";
+	return (
 		<>
-		<div className="startpage">
-			<div className="description-text text-start">
-				{textPart1} <br/> {textPart2}
-			</div>
-		</div>
 			<main className="startpage-main">
 				<Link to="/battle">
-					<MenuCard color='peach' text='Battle' img={BattleImg} />
+					<MenuCard color="peach" text="Battle" img={BattleImg} />
 				</Link>
 				<Link to="/stats">
-					<MenuCard color='teal' text='Statistics' img={StatisticsImg}/>
+					<MenuCard
+						color="teal"
+						text="Statistics"
+						img={StatisticsImg}
+					/>
 				</Link>
 				<Link to="/upload">
-					<MenuCard color='pink' text='Add your hamster' img={AddImg} />
+					<MenuCard
+						color="pink"
+						text="Add hamster"
+						img={AddImg}
+					/>
 				</Link>
-
 			</main>
+			<div className="startpage">
+				<div className="description-text text-start">
+					<p>{textPart1}</p>
+					<p>{textPart2}</p>
+					<p className="textpart3">{textPart3}</p>
+				</div>
+			</div>
 		</>
-	)
+	);
 }
 
 export default StartPage;
