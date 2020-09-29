08073b81fe784f310a6b6775089e94e68c5a5519
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2020-09-30 00:46:18 +0200

    Removed middleware

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(
         );
 app.use(express.static(__dirname + "/../build/"));
 app.use(express.static(__dirname + "/../src/assets/"));
-app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+// app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 // app.use(express.static(__dirname + "/../public/"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
