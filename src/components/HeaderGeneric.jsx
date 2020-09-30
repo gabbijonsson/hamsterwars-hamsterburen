f8fbbe6f1890e9ed0532ec343eafb66a2111861e
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2020-09-30 15:23:53 +0200

    Changed text on pickfighters button

--- src/components/HeaderGeneric.jsx
+++ src/components/HeaderGeneric.jsx
@@ -8,29 +8,29 @@ import {Link} from 'react-router-dom';
 
 function HeaderGeneric({content}) {
 	return (
-		<header id='HeaderGeneric'>
+		<header id="HeaderGeneric">
 			<div className="header-item-1">
 				<Link to="/">
-					<img src={HomeIcon} alt="App icon (to go home)"/>
+					<img src={HomeIcon} alt="App icon (to go home)" />
 				</Link>
 
-				
 				<div className="desktop-nav">
 					<HamburgerMenu />
 				</div>
 			</div>
-			<div className="header-item-2">
-
-			</div>
+			<div className="header-item-2"></div>
 			<div className="header-item-3">
 				<HamburgerMenuBtn />
-				<div className={'desktop-header-btn ' + content}>
-					<DesktopHeaderBtn text="PICK YOUR OWN FIGHTERS" color="teal" link="/pickfighters"  />
+				<div className={"desktop-header-btn " + content}>
+					<DesktopHeaderBtn
+						text="SELECT COMBATANTS"
+						color="teal"
+						link="/pickfighters"
+					/>
 				</div>
 			</div>
-			
 		</header>
-	)
+	);
 }
 
 export default HeaderGeneric;
